91c35a5ec6f2452a3b656fcf1da7701130b3f106
cagecorn <lmcbv@naver.com>
2025-07-20 04:17:36 +0900

    Fix pixelation issues

--- src/game/main.js
+++ src/game/main.js
@@ -22,6 +22,9 @@ const config = {
     height: surveyEngine.canvas.height,
     parent: 'game-container',
     backgroundColor: '#000000',
+    // --- 수정 및 추가 ---
+    pixelArt: true, // 픽셀 아트 모드를 활성화하여 뭉개짐을 방지합니다.
+
     // 기기의 픽셀 비율에 따라 해상도를 자동으로 조정합니다.
     resolution: window.devicePixelRatio || 1,
     scale: {
